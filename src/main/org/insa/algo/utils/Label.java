4126b6641e29d30e076b253b346520b38b0e4650
M. Andrieu Jonathan <jandrieu@insa-08128>
2019-05-06 08:41:29 +0200

    espace OK

--- src/main/org/insa/algo/utils/Label.java
+++ src/main/org/insa/algo/utils/Label.java
@@ -3,16 +3,13 @@ package org.insa.algo.utils;
 import org.insa.graph.Node;
 
 public class Label {
+		
 	int sommet_courant;
 	boolean marque;
 	int cout;
 	Node pere;
-
 	
-	
-	
-	
-int getcost() {
+	int getcost() {
 	return this.cout;
-}
+	}
 }
