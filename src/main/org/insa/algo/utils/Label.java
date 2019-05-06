46a28f17c6dee437f974960736530461c0494991
M. Mahraye Abderrazzak <mahraye@insa-08129>
2019-05-06 08:35:14 +0200

    Label_ajoute

---
+++ src/main/org/insa/algo/utils/Label.java
@@ -0,0 +1,18 @@
+package org.insa.algo.utils;
+
+import org.insa.graph.Node;
+
+public class Label {
+	int sommet_courant;
+	boolean marque;
+	int cout;
+	Node pere;
+
+	
+	
+	
+	
+int getcost() {
+	return this.cout;
+}
+}
