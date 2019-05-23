ba62815193101de39a56ddc12397157c05e9bd9e
Jonathan Andrieu <jonathan.andrieu11@gmail.com>
2019-05-23 13:21:31 +0200

    la belle la star

--- src/main/org/insa/algo/utils/LabelStar.java
+++ src/main/org/insa/algo/utils/LabelStar.java
@@ -1,19 +1,40 @@
 package org.insa.algo.utils;
  
 import org.insa.graph.Node;
-import org.insa.graph.Arc;
+import org.insa.graph.Point;
+import org.insa.algo.AbstractInputData.Mode;
+import org.insa.algo.shortestpath.ShortestPathData;
 
 public class LabelStar extends Label {
 	
-	private Label lb ;
+	private double totCost ;
 	
-	public LabelStar (Node courant) {
-	super(courant);
+	public LabelStar (Node courant, ShortestPathData data) {
+		super(courant);
+		
+		if(data.getMode() == Mode.LENGTH) {
+			double lg = Point.distance(courant.getPoint(), data.getDestination().getPoint()) ;
+			this.setTotCost(lg);
+			}
+		else {
+			double v = data.getMaximumSpeed() ;
+			double lg = Point.distance(courant.getPoint(), data.getDestination().getPoint()) ;
+			double tm = lg / v;
+			this.setTotCost(tm) ;
+		}
+	}
+
+	public double getTotCost() {
+		return totCost;
+	}
+
+	public void setTotCost(double totCost) {
+		this.totCost = totCost;
 	}
 	
-	public double getTotalCost() {
-		return lb.getCost()+lb.getTotalCost();
-	}
+	
+	
+	
 }
 
 
